f4ff98769717b149991be3fd7a2709cd45124b3a
Roshan Thapa <thaparoshan3722@gmail.com>
2023-12-31 19:15:08 +0545

    add: Home page

---
+++ frontend/src/page/Home.tsx
@@ -0,0 +1,10 @@
+
+const Home : React.FC = () => {
+  return (
+    <div className="flex flex-col justify-evenly items-center min-h-[100vh] bg-black text-white">
+        <h1 className="text-theme text-6xl font-bold">Stockwatch</h1>
+    </div>
+  )
+}
+
+export default Home
