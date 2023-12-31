ae1a00c0d686c9f8aa5a8b24f6dc4867ed598373
Roshan Thapa <thaparoshan3722@gmail.com>
2023-12-31 19:14:45 +0545

    add: Navbar and Footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,11 @@
 
-function Footer()
-{
-	return(
-		<div className="Footer">Footer</div>
-	)
+
+const Footer : React.FC = () => {
+  return (
+	<div className="flex flex-col justify-end items-center min-h-[40vh] bg-black p-2 text-white">
+		<span className="text-sm font-light m-2">All copyright &copy; reserved | Stockwatch</span>
+	</div>
+  )
 }
 
 export default Footer
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,33 @@
+import { Link } from "react-router-dom";
+import { _NavItems } from "../assets/data/stockwatch";
 
-function Navbar()
+interface INavItem {
+	item : string,
+	url : string,
+};
+
+const Navbar : React.FC = () => {
+	const logoPath = require("../assets/images/logo.png");
+
+  return (
+	<nav className="flex flex-row items-center justify-between w-[100%] fixed z-20 p-2">
+		<img src={logoPath} alt={"logo"} className="w-[4rem] mx-8"/>
+		<ul className="flex flex-row items-center justify-evenly w-[30%] text-theme-alth">
+			{
+				_NavItems.map(({item, url})=>
+				{
+					return <NavItem item={item} url={url} />
+				})
+			}
+		</ul>
+	</nav>
+  )
+}
+
+const NavItem : React.FC<INavItem> = ({item, url}) =>
 {
-	return(
-		<div className="Navbar">Navbar</div>
+	return (
+		<Link to={url} className="p-2 m-2 underline-offset-8 hover:underline hover:text-theme-alt text-xl transition-all duration-300">{item}</Link>
 	)
 }
 
