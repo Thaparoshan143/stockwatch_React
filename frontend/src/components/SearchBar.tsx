25988f8865e1a729b86300301004fc31e693684b
Roshan Thapa <thaparoshan3722@gmail.com>
2024-05-04 13:47:11 +0545

    update: Search bar placeholder

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -9,7 +9,7 @@ const SearchBar : React.FC = () => {
 
   return (
     <div className="w-[60%] flex flex-row justify-between items-center bg-white rounded-[2rem] overflow-hidden min-h-[3rem] h-[4rem]">
-      <input type="text" className="h-[100%] px-8 outline-none w-[100%] text-theme text-xl" placeholder="Enter script here..." onChange={(e)=>setInpURL(e.target.value)}/>
+      <input type="text" className="h-[100%] px-8 outline-none w-[100%] text-theme text-xl" placeholder="Enter script name here..." onChange={(e)=>setInpURL(e.target.value)}/>
       <Link to={window.location.href + "script/" + inpURL} className="text-white bg-theme h-[100%] px-6">
         <FaSearch className="text-3xl h-[100%]" />
       </Link>
